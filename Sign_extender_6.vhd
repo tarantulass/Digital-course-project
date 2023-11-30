cc1da11c10e09b658795f84cb3dfef2645466780
tarantulass <138518455+tarantulass@users.noreply.github.com>
2023-11-30 17:54:05 +0530

    Update Sign_extender_6.vhd

--- Sign_extender_6.vhd
+++ Sign_extender_6.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 entity sign_extend_6 is
 	port (inp: in std_logic_vector(5 downto 0); extended: out std_logic_vector(15 downto 0));
-end entity sign_extend_10;
+end entity sign_extend_6;
 
 architecture main of sign_extend_6 is
 	
