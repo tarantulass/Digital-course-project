91d1e948545aa2cd24350aa34deb98eb14ea1765
tarantulass <138518455+tarantulass@users.noreply.github.com>
2023-11-30 17:54:21 +0530

    Update Sign_extender_9.vhd

--- Sign_extender_9.vhd
+++ Sign_extender_9.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 entity sign_extend_9 is
 	port (inp: in std_logic_vector(8 downto 0); extended: out std_logic_vector(15 downto 0));
-end entity sign_extend_10;
+end entity sign_extend_9;
 
 architecture main of sign_extend_9 is
 	
