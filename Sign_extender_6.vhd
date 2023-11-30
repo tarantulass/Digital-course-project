0b07c7774a332544aa54631693c549d12e1a2be7
tarantulass <138518455+tarantulass@users.noreply.github.com>
2023-11-30 16:51:52 +0530

    Sign extender 6 bits

---
+++ Sign_extender_6.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+use ieee.numeric_std.all;
+
+entity sign_extend_6 is
+	port (inp: in std_logic_vector(5 downto 0); extended: out std_logic_vector(15 downto 0));
+end entity sign_extend_10;
+
+architecture main of sign_extend_6 is
+	
+	begin
+
+	extended <= "1111111111" & inp when (inp(5) = '1') else
+				"0000000000" & inp;
+
+end main;
